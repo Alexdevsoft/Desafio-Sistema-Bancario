a8e5d9c8d22ca17bcf8a9f9afa3e901b5abab070
Miladevsoft <milahavila@gmail.com>
2024-06-21 20:01:53 -0300

    Sistema bancario v1

---
+++ desafio_v1.py
@@ -0,0 +1,78 @@
+from abc import ABC, abstractclassmethod, abstractproperty
+from datetime import datetime
+
+
+class Cliente:
+    def __init__(self, endereco):
+        self.endereco = endereco
+        self.contas = []
+
+    def realizar_transacao(self, conta, transacao):
+        transacao.registrar(conta)
+
+    def adiconar_conta(self, conta):
+        self.conas.append(conta)
+
+
+class PessoaFisica(cliente):
+    def __init__(self, nome, data_nascimento, cpf, endereco):
+        super().__init__(endereco)
+        self.nome = nome
+        self.data_nascimento = data_nascimento
+        self.cpf = cpf
+
+
+class Conta:
+    def __init__(self, numero, client):
+        self._saldo = 0
+        self._numero = numero
+        self._agencia = "0001"
+        self._client = client
+        self._historico = Historico()
+
+    @classmethod
+    def nova_conta(cls, client, numero):
+        return cls(numero, client)
+
+    @property
+    def saldo(self):
+        return self._saldo
+
+    @property
+    def numero(self):
+        return self._numero
+
+    @property
+    def agencia(self):
+        return self._agencia
+
+    @property
+    def client(self):
+        return self._client
+
+    @property
+    def historico(self):
+        return self._historico
+
+    @property
+    def sacar(self, valor):
+        saldo = self.saldo
+        excedeu_saldo = valor > saldo
+
+        if excedeu_saldo:
+            print("\n@@@ Operação falhou! Você não tem saldo suficiente. @@@")
+        elif valor > 0:
+            self._saldo -= valor
+            print("\n=== Saque realizado com sucesso! ===")
+            return True
+        else:
+            print("\n@@@ Operação falhou! O valor informado é inválido. @@@")
+
+    def depositar(self, valor):
+        if valor > 0:
+            self._saldo += valor
+            print("\n=== Depósito realizado com sucesso! ===")
+        else:
+            print("\n@@@ Operação falhou! O valor informado é inválido. @@@")
+
+            return True
